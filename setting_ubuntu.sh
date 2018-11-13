d612d0f39bc9c6671a4b1bc4eeed7dd390c735c6
mrwangyu2 <wangyuxxx@163.com>
2018-11-13 12:13:14 +0800

    Update for configur vim

--- setting_ubuntu.sh
+++ setting_ubuntu.sh
@@ -1,25 +1,9 @@
 #/bin/bash
 
-TITLE="Setting Ubuntu16.04LTS"
+TITLE="Setting Ubuntu16.04 Server LTS"
 
 function main(){
   echo_message header "Starting main function"
-  
- # MAIN=$(eval `resize` && whiptail \
- #        --notags \
- #        --title "$TITLE" \
- #        --menu "\nWhat would you like to do?" \
- #        --cancel-button "Quit" \
- #        $LINES $COLUMNS $(( $LINES -12 )) \
- #        'system_update'     'Perfor system updates' \
-#         'install_app'            'Install preferred applications' \
-#         'install_terminal_tools' 'Install terminal tools' \
-#         'install_dev'            'Install preferred development tools'
-#         'system_configure'       'Configure system' \
-#         'system_cleanup'         'Cleanup the system' \
-         #3>&1 1>&2 2>&3)
-
-#         "6"         "Cleanup the system" \
 
   MAIN=$(eval `resize` && whiptail \
          --notags \
@@ -30,7 +14,7 @@ function main(){
          "system_update"     "Update software source." \
          "install_apps"      "Install preferred applications" \
          "download_tar_packages" "Download preferred tar packages" \
-         "system_configure"  "Configure system" \
+         "system_configure"  "Configure system evironment" \
          3>&1 1>&2 2>&3)
 
   if [[ $? = 0 ]]; then
