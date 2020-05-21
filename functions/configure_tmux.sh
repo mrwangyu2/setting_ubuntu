0915f9f8e582b161623917c741fa9a0fbda7801a
mrwangyu2 <wangyuxxx@163.com>
2020-05-21 19:07:22 +0800

    Update for tmux

--- functions/configure_tmux.sh
+++ functions/configure_tmux.sh
@@ -9,6 +9,7 @@ function install_plugins(){
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
   cp ./data/tmux_script/tmux.conf ~/.tmux.conf
   tmux
+  # prefix + I to install plugins
   tmux source ~/.tmux.conf
   cp ./data/tmux_script/default.sh ~/.tmux/plugins/tmux-powerline/themes/.
 } 
